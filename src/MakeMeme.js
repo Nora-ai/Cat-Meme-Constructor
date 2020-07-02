2f8ccf523ad0a3c03634bb23b8ccb7919738dcb4
Ellenora Tulchina <snora@MacBook-Pro.local>
2020-07-02 17:19:55 -0400

    border edits

--- src/MakeMeme.js
+++ src/MakeMeme.js
@@ -1,9 +1,10 @@
 import React, { Component, useState } from 'react'
-import { useParams } from 'react-router-dom'
+//import { useParams } from 'react-router-dom'
 import AddCaption from './AddCaption'
 
 export default function MakeMeme (props) {
     let [borderColor, updateBorder] = useState("")
+    let [filter, updateFilter] = useState("")
     //let params = useParams()
 
     const animalMeme = props.history.location.pathname
@@ -14,12 +15,16 @@ export default function MakeMeme (props) {
         updateBorder(color)
     }
     
+    
+
     return (<>
     <h1 className="custom-header">Customization Station</h1>
          <div className="pic-select">
              <div className={borderColor}><img src={animalLink} alt="animalimage"></img></div>
          </div>
          <AddCaption />
-         <button onClick={() => handleClick('blue')}>Add Pink Border</button>
+         <button onClick={() => handleClick('pink')}>Add Pink Border</button>
+         <button onClick={() => handleClick('blue')}>Add blue Border</button>
+         <button onClick={() => handleClick('green')}>Add green Border</button>
     </>)
 }   
