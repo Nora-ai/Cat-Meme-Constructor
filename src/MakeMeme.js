0b908dcaa3e260a592fd254a6cbc14b9c09548dc
Ellenora Tulchina <snora@MacBook-Pro.home>
2020-07-02 15:50:27 -0400

    border colors work

--- src/MakeMeme.js
+++ src/MakeMeme.js
@@ -3,23 +3,23 @@ import { useParams } from 'react-router-dom'
 import AddCaption from './AddCaption'
 
 export default function MakeMeme (props) {
-    // let [border, updateBorder] = useState("")
-    const params = useParams()
+    let [borderColor, updateBorder] = useState("")
+    //let params = useParams()
 
     const animalMeme = props.history.location.pathname
     console.log(animalMeme.substring(10, animalMeme.length))
     const animalLink = animalMeme.substring(10, animalMeme.length)
 
-    // updateBorder({
-
-    // })
-
+    const handleClick = (color) => {
+        updateBorder(color)
+    }
+    
     return (<>
     <h1 className="custom-header">Customization Station</h1>
          <div className="pic-select">
-             <div className="pink"><img src={animalLink} alt="animalimage"></img></div>
+             <div className={borderColor}><img src={animalLink} alt="animalimage"></img></div>
          </div>
          <AddCaption />
-         {/* <button onClick={updateBorder('pink')}>Add Pink Border</button> */}
+         <button onClick={() => handleClick('blue')}>Add Pink Border</button>
     </>)
 }   
